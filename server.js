c55a9a795be259536bd23d0a1e81b9d25541559a
Danny <jacquez.j.daniel@gmail.com>
2019-07-21 19:06:59 -0700

    ssl

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var enforce = require("express-sslify");
 const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -13,8 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.use(cookieParser());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
