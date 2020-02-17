fbf808196abd6bc95d5a80172ce12f28389ec46e
Danny <jacquez.j.daniel@gmail.com>
2020-02-16 19:25:38 -0800

    speed

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,11 @@
 import thunkMiddleware from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
-import { loadingBarMiddleware } from "react-redux-loading-bar";
 
 import rootReducer from "./reducers";
 
-const createStoreWithMiddleware = compose(
-  applyMiddleware(
-    thunkMiddleware,
-    loadingBarMiddleware({
-      promiseTypeSuffixes: ["REQUEST", "SUCCESS", "FAILURE"]
-    })
-  )
-)(createStore);
+const createStoreWithMiddleware = compose(applyMiddleware(thunkMiddleware))(
+  createStore
+);
 
 const store = createStoreWithMiddleware(rootReducer);
 
