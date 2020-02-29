951d4166518c27e387b96962baf88861718035d3
Danny <jacquez.j.daniel@gmail.com>
2020-02-28 22:03:13 -0800

    contact verbage

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -47,73 +47,77 @@ class contact extends Component {
   render() {
     return (
       <div className="container">
-        <ScrollToTop showUnder={160}>
-          <i class="material-icons prefix md-36 orange600">arrow_upward</i>
-          <span for="icon_prefix"></span>
-        </ScrollToTop>
-        {/* <h5>Contact Us</h5> */}
-        <br />
-        <br />
-
-        <div id="contact-form" className="container">
-          <form
-            name="contact-form"
-            id="contact-form"
-            onSubmit={this.handleSubmit.bind(this)}
-            method="POST"
-          >
-            <i class="material-icons prefix">account_circle</i>
-            <label for="icon_prefix"></label>
-            <input
-              type="text"
-              id="icon_prefix"
-              name="name"
-              validate="true"
-              required
-              value={this.state.name}
-              placeholder="Full Name"
-              onChange={this.handleChange}
-            />
-            <i class="material-icons prefix">mail_outline</i>
-            <label for="icon_prefix"></label>
-            <input
-              type="email"
-              id="email"
-              class="validate"
-              name="email"
-              required
-              value={this.state.email}
-              placeholder="Your email.."
-              onChange={this.handleChange}
-            />
-            <span
-              class="helper-text"
-              data-error="Please Enter A Valid Email. ❌"
-              data-success="✔️"
-            ></span>
-            <br></br>
-            <br></br>
-            <i class="material-icons prefix">message</i>
-            <label for="icon_prefix"></label>
-            <textarea
-              id="message"
-              name="message"
-              class="validate"
-              required
-              value={this.state.message}
-              placeholder="Your Message.."
-              onChange={this.handleChange}
-            />
-            <button
-              class="btn waves-effect waves-light indigo accent-4"
-              disabled={this.state.disableButton}
-              name="action"
-              type="submit"
-              value="Submit"
+        <div className="container">
+          <ScrollToTop showUnder={160}>
+            <i class="material-icons prefix md-36 orange600">arrow_upward</i>
+            <span for="icon_prefix"></span>
+          </ScrollToTop>
+          <div id="contact-form" className="container">
+            <h4>Contact Us</h4>
+            <br />
+            <p>
+              Please fill out the contact form below and we will reply as soon
+              as possible.
+            </p>
+            <form
+              name="contact-form"
+              id="contact-form"
+              onSubmit={this.handleSubmit.bind(this)}
+              method="POST"
             >
-              <i class="material-icons right">send</i>Send
-            </button>
-          </form>
+              <i class="material-icons prefix">account_circle</i>
+              <label for="icon_prefix"></label>
+              <input
+                type="text"
+                id="icon_prefix"
+                name="name"
+                validate="true"
+                required
+                value={this.state.name}
+                placeholder="Full Name"
+                onChange={this.handleChange}
+              />
+              <i class="material-icons prefix">mail_outline</i>
+              <label for="icon_prefix"></label>
+              <input
+                type="email"
+                id="email"
+                class="validate"
+                name="email"
+                required
+                value={this.state.email}
+                placeholder="Your email.."
+                onChange={this.handleChange}
+              />
+              <span
+                class="helper-text"
+                data-error="Please Enter A Valid Email. ❌"
+                data-success="✔️"
+              ></span>
+              <br></br>
+              <br></br>
+              <i class="material-icons prefix">message</i>
+              <label for="icon_prefix"></label>
+              <textarea
+                id="message"
+                name="message"
+                class="validate"
+                required
+                value={this.state.message}
+                placeholder="Your Message.."
+                onChange={this.handleChange}
+              />
+              <button
+                class="btn waves-effect waves-light indigo accent-4"
+                disabled={this.state.disableButton}
+                name="action"
+                type="submit"
+                value="Submit"
+              >
+                <i class="material-icons right">send</i>Send
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );
